b59558adbe24c6cf9245f76f3dc537fb11aaa5f7
morshidulrahman <boydanger416@gmail.com>
2024-07-17 00:22:28 +0600

    transaction add

--- src/app/components/dashboard/agent/AgentTable.js
+++ src/app/components/dashboard/agent/AgentTable.js
@@ -1,7 +1,29 @@
+import axios from "axios";
 import React from "react";
+import toast from "react-hot-toast";
 
 const AgentTable = ({ requestLists, refetch }) => {
-  const { status, transactionType, amount, mynumber } = requestLists;
+  const { status, transactionType, amount, mynumber, _id } = requestLists;
+
+  const transactionlist = async () => {
+    const userInfo = {
+      status: "checked",
+      amount,
+      transactionType,
+      mynumber,
+      transaction: _id,
+    };
+    const { data } = await axios.post(
+      "http://localhost:5000/transictionlist",
+      userInfo
+    );
+    console.log(data);
+    return data;
+  };
+
+  const acceptTransactions = async () => {
+    transactionlist();
+  };
 
   return (
     <tr>
@@ -11,15 +33,15 @@ const AgentTable = ({ requestLists, refetch }) => {
       <td className="px-12 py-4 text-sm font-medium text-gray-700 whitespace-nowrap">
         {amount}
       </td>
-      <td className="px-4 py-4 text-sm text-gray-500   whitespace-nowrap">
+      <td className="px-4 py-4 text-sm text-gray-500 font-medium pl-16 whitespace-nowrap capitalize">
         {transactionType}
       </td>
-      <td className="px-4 py-4 text-sm text-gray-500   whitespace-nowrap">
+      <td className="px-4 py-4 text-sm text-gray-500 font-medium  whitespace-nowrap">
         {status}
       </td>
-      <td className="px-4 py-4 text-sm text-gray-500   whitespace-nowrap flex gap-1">
+      <td className="px-4 py-4 text-sm text-gray-500 font-medium  whitespace-nowrap  ">
         <button
-          onClick={""}
+          onClick={acceptTransactions}
           className="px-3 py-2 text-sm bg-green-500 text-white rounded-full flex items-center justify-center font-semibold duration-300 transition-all hover:scale-105"
         >
           Accept
